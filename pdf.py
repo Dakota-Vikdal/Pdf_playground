1aa47180071e937b0cf0034cc256c7b19b6f6988
Dakota Vikdal <dakota.james19@hotmail.com>
2023-10-05 21:10:30 -0400

    variables for merged files, watermark file and combining the two. Loop over merged file and add watermark file

--- pdf.py
+++ pdf.py
@@ -1,3 +1,13 @@
 import PyPDF2
 
+# We want to grab the merged pdf files as well as the watermark file and combine them.
+merged = PyPDF2.PdfFileReader(open('merged.pdf', 'rb'))
+watermark = PyPDF2.PdfFileReader(open('wtr.pdf', 'rb'))
+output = PyPDF2.PdfFileWriter()
 
+# Next we loop through the pages of the merged file and add the watermark to each page
+for i in range(merged.getNumPages()):
+    page = merged.getPage(i)
+    page.mergePage(watermark.getPage(0))
+    output.addPage(page)
+    
